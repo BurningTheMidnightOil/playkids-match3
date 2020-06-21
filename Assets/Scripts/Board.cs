6d10aef75fb795dce3811e29f3087202e7cb9584
XeIN <btmo4444@gmail.com>
2020-06-20 21:15:27 -0300

    board setup logic with number of tiles and camera resizes depending on number of tiles

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+
+    public int lateralSize;
+
+    public GameObject tilePrefab;
+    public Tile[,] allTiles;
+
+    void Start()
+    {
+        allTiles = new Tile[width, height];
+        SetupTiles();
+        SetupCamera();
+    }
+
+    void SetupTiles()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                GameObject tile = Instantiate(tilePrefab, new Vector3(i, j, 0), Quaternion.identity, transform);
+                allTiles[i,j] = tile.GetComponent<Tile>();
+            }
+        }
+    }
+
+    void SetupCamera()
+    {
+        Camera.main.transform.position = new Vector3((float) (width - 1) / 2f, (float) (height - 1) / 2f, -10f);
+        float aspectRatio = (float) Screen.width / (float) Screen.height;
+        float verticalSize = (float)height / 2f + (float)lateralSize;
+        float horizontalSize = ((float)width / 2f + (float)lateralSize) / aspectRatio;
+
+        Camera.main.orthographicSize = (verticalSize > horizontalSize) ? verticalSize : horizontalSize;
+    }
+
+}
